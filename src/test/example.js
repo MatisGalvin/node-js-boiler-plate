53c5cb7fe3835bc12773611300b25501b613ae7d
rlebhar <robin.lebhar.chill.studio@gmail.com>
2022-01-04 10:56:18 +0100

    chore(initalize-project): Intialize boiler plate

---
+++ src/test/example.js
@@ -0,0 +1,8 @@
+import assert from "assert";
+
+describe("Test du create", () => {
+  it("Sauvegarde un livre et s'assure qu'il est bien sauvé", (done) => {
+    assert(1 === 1);
+    done();
+  });
+});
